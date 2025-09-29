41c42227249ce731ff039d12ec65068980eec46a
Carlos Leonardo de Souza Oliveira <carlos.so@fieb.org.br>
2025-09-29 18:16:50 -0300

    Primeiro commit

---
+++ terraform-aws-ec2/variables.tf
@@ -0,0 +1,40 @@
+variable "aws_regiao" {
+  description = "Região da AWS"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "tipo_instancia_master" {
+  description = "Tipo da instância EC2"
+  type        = string
+  default     = "t3a.small"
+}
+
+variable "tipo_instancia_workers" {
+  description = "Tipo da instância EC2"
+  type        = string
+  default     = "t3.mciro"
+}
+
+variable "nome_ssh_key" {
+  description = "Nome da chave SSH já criada na AWS"
+  type        = string
+}
+
+variable "tamanho_volume" {
+  description = "Tamanho do disco EBS em GB"
+  type        = number
+  default     = 20
+}
+
+variable "projeto" {
+  description = "Projeto BRY"
+  type        = string
+  default     = "k8s-BRY"
+}
+
+variable "versao_terraform" {
+  description = "Versão do Terraform"
+  type = string
+  default = "1.5.0"
+}
